91042a3ec2e39b1706f1d40a15073bb3a8afcd66
Aditi-panta <88805173+Aditi-panta@users.noreply.github.com>
2023-04-15 14:55:45 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
+
 function preload(){
     song=loadSound("music.mp3");
-    song=loadSound("music2.mp3");
+    song1=loadSound("music2.mp3");
 }
 
 function setup(){
@@ -8,6 +13,22 @@ canvas=createCanvas(600,500);
 canvas.center();
 video=createCapture(VIDEO);
 video.hide();
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log("poseNet is initialized");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("right wrist x="+rightWristX+"right wrist y="+rightWristY);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("left wrist x="+leftWristX+"left wrist y="+leftWristY);
+    }
 }
 function draw(){
     image(video,0,0,600,500);
